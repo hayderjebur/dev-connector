825274479b283d4b4c3fc558718f0f6e1d171360
Hayder Jebur <hayderjabur1983@gmail.com>
2020-08-27 11:22:09 -0700

    restage

--- server.js
+++ server.js
@@ -17,13 +17,14 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+// "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
